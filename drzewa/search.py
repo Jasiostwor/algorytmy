ff1d3cac94dbd2f7085069176992946685396100
jakubkaminski167951 <jakub.kaminski.2@student.put.poznan.pl>
2026-04-08 19:12:08 +0200

    eewa

--- drzewa/search.py
+++ drzewa/search.py
@@ -1,23 +1,7 @@
-def get_inorder(node, result):
-    """
-    Przechodzi drzewo w kolejności: Lewy, Korzeń, Prawy.
-    Wynik w BST jest zawsze posortowany rosnąco.
-    """
-    if node is None:
-        return
-    
-    get_inorder(node.left, result)
-    result.append(str(node.value))
-    get_inorder(node.right, result)
+def get_min_path(node):
+    path = []
+    return path
 
-def get_preorder(node, result):
-    """
-    Przechodzi drzewo w kolejności: Korzeń, Lewy, Prawy.
-    Używane często do kopiowania struktury drzewa.
-    """
-    if node is None:
-        return
-    
-    result.append(str(node.value))
-    get_preorder(node.left, result)
-    get_preorder(node.right, result)
+def get_max_path(node):
+    path = []
+    return path
