e609c82fb303fc7a786ed92e03f23f644d7278e4
Jasiostwor <jasiostwor1@gmail.com>
2026-05-07 13:29:40 +0200

    przechodzenie

--- grafy/traversals.py
+++ grafy/traversals.py
@@ -1,9 +1,47 @@
+from collections import deque
+
 def bfs(graph):
-    """Przechodzenie wszerz (BFS) - do zaimplementowania."""
-    if graph.n == 0: return "Graf jest pusty."
-    return "[TODO] Przechodzenie wszerz (BFS) nie zostało jeszcze zaimplementowane."
+    """Przechodzenie wszerz (BFS)."""
+    if graph.n == 0: 
+        return "Graf jest pusty."
+    
+    visited = [False] * graph.n
+    result = []
+    
+    for start_node in range(graph.n):
+        if not visited[start_node]:
+            queue = deque([start_node])
+            visited[start_node] = True
+            
+            while queue:
+                u = queue.popleft()
+                result.append(str(u))
+                
+                for v in graph.succ_list[u]:
+                    if not visited[v]:
+                        visited[v] = True
+                        queue.append(v)
+                        
+    return " ".join(result)
 
 def dfs(graph):
-    """Przechodzenie w głąb (DFS) - do zaimplementowania."""
-    if graph.n == 0: return "Graf jest pusty."
-    return "[TODO] Przechodzenie w głąb (DFS) nie zostało jeszcze zaimplementowane."
+    """Przechodzenie w głąb (DFS)."""
+    if graph.n == 0: 
+        return "Graf jest pusty."
+    
+    visited = [False] * graph.n
+    result = []
+    
+    def dfs_visit(u):
+        visited[u] = True
+        result.append(str(u))
+        
+        for v in graph.succ_list[u]:
+            if not visited[v]:
+                dfs_visit(v)
+                
+    for start_node in range(graph.n):
+        if not visited[start_node]:
+            dfs_visit(start_node)
+            
+    return " ".join(result)
